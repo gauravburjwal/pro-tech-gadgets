1f09c57bc00ecdbb1df3a9efa92ae8409fa5f472
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-05-15 00:43:02 +0530

    ✨Add CollectionPreview on shop page

--- src/components/CollectionPreview.jsx
+++ src/components/CollectionPreview.jsx
@@ -1,4 +1,5 @@
 import '../styles/CollectionPreview.scss';
+import CollectionItem from './CollectionItem';
 
 function CollectionPreview({ title, items }) {
     return (
@@ -7,8 +8,8 @@ function CollectionPreview({ title, items }) {
             <div className='preview'>
                 {items
                     .filter((item, idx) => idx < 4)
-                    .map((item) => (
-                        <div key={item.id}>{item.name}</div>
+                    .map(({ id, ...otherItemProps }) => (
+                        <CollectionItem key={id} {...otherItemProps} />
                     ))}
             </div>
         </div>
