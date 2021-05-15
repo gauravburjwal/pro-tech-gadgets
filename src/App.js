bfe2fe2e7f13dbd249bdffe6444ab1efd092aa81
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-05-15 13:27:20 +0530

    ✨Add header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './components/HomePage';
 import { ShopPage } from './components/ShopPage';
+import Header from './components/Header';
 
 const App = () => {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/shop' component={ShopPage} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import '../styles/Header.scss';
+import { ReactComponent as Logo } from '../assets/pro-tech-gadgets.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div className='header'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo' />
+            </Link>
+
+            <div className='options'>
+                <Link className='option' to='/shop'>
+                    SHOP
+                </Link>
+
+                <Link className='option' to='/contact'>
+                    CONTACT
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
